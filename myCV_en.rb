1b16c9bfe7f0da1e78597b51e2552fb36f807b3b
Nicolas Morey <nmorey@suse.com>
2025-10-23 18:03:35 +0200

    myCV: Drop GAnalytics
    Signed-off-by: Nicolas Morey <nmorey@suse.com>

--- myCV_en.rb
+++ myCV_en.rb
@@ -340,21 +340,6 @@ cv.middleStuff="
                 <a href=\"photos/\">Take a look at my pictures from hikes and holidays!!</a><br>
 "
 cv.footer = "
-<script type=\"text/javascript\">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-21010187-1']);
-  _gaq.push(['_setDomainName', 'morey-chaisemartin.com']);
-  _gaq.push(['_setAllowLinker', true]);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 <div style=\"font-size:0px\">"
 
 cv.fuzzy_block(cv.categories(), cv.keywords(), 1) {|prefix, keyword|
